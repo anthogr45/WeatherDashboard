4acd22dfcdc2992baad8d312f27917a5978194b2
AnthoG <anthony.dswg@gmail.com>
2023-09-17 16:20:41 -0400

    Structure was done

---
+++ assets/script/script.js
@@ -0,0 +1,68 @@
+
+
+var cityName = document.querySelector("#cityName");
+var cityBtn = document.querySelector("#btnSearch");
+
+var mboxCityName = document.querySelector("#cityNameBmx");
+var mboxTemp = document.querySelector("#tempBxm");
+var mboxWind = document.querySelector("#windBxm");
+var mboxHumid = document.querySelector("#humidBxm");
+
+var day1Temp = document.querySelector("#tempBx1");
+var day1Wind = document.querySelector("#windBx1");
+var day1Humid = document.querySelector("#humidBx1");
+
+var day2Temp = document.querySelector("#tempBx2");
+var day2Wind = document.querySelector("#windBx2");
+var day2Humid = document.querySelector("#humidBx2");
+
+var day2Temp = document.querySelector("#tempBx2");
+var day2Wind = document.querySelector("#windBx2");
+var day2Humid = document.querySelector("#humidBx2");
+
+var day3Temp = document.querySelector("#tempBx3");
+var day3Wind = document.querySelector("#windBx3");
+var day3Humid = document.querySelector("#humidBx3");
+
+var day4Temp = document.querySelector("#tempBx4");
+var day4Wind = document.querySelector("#windBx4");
+var day4Humid = document.querySelector("#humidBx4");
+
+var day5Temp = document.querySelector("#tempBx5");
+var day5Wind = document.querySelector("#windBx5");
+var day5Humid = document.querySelector("#humidBx5");
+
+
+
+
+
+// var a 
+// console.log("TEST") 
+// // cityBtn.addEventListener("click", loaddata)
+
+
+
+
+//  function loaddata () {
+//   // var getvalue = function (event) {
+//   a = "lcmsmc"
+//     // preventDefault();  
+//     // var datatxt = cityName.value(); 
+//     console.log("TEST") 
+    
+// // console.log(a)
+
+// }
+
+// cityBtn.addEventListener("click", loaddata);
+
+// function () {
+    // var getvalue = function (event) {
+
+      // preventDefault();  
+      // var datatxt = cityName.value(); 
+      //console.log("TEST") 
+      
+  // console.log(a)
+  
+//   });
